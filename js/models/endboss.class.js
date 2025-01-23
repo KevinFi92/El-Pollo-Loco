ff398553ba4637817b2ba800c5ade6620aeaf26d
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-01-23 20:47:43 +0100

    fixed endboss

--- js/models/endboss.class.js
+++ js/models/endboss.class.js
@@ -3,10 +3,14 @@ class Endboss extends MoveableObject{
     width = 200;
     currentImg = 0;
     movingImg = [
-        "img/4_enemie_boss_chicken/1_walk/G1.png",
-        "img/4_enemie_boss_chicken/1_walk/G2.png",
-        "img/4_enemie_boss_chicken/1_walk/G3.png",
-        "img/4_enemie_boss_chicken/1_walk/G4.png"
+        "img/4_enemie_boss_chicken/2_alert/G5.png",
+        "img/4_enemie_boss_chicken/2_alert/G6.png",
+        "img/4_enemie_boss_chicken/2_alert/G7.png",
+        "img/4_enemie_boss_chicken/2_alert/G8.png",
+        "img/4_enemie_boss_chicken/2_alert/G9.png",
+        "img/4_enemie_boss_chicken/2_alert/G10.png",
+        "img/4_enemie_boss_chicken/2_alert/G11.png",
+        "img/4_enemie_boss_chicken/2_alert/G12.png",
       ];
       constructor(){
         super().loadImg("img/4_enemie_boss_chicken/1_walk/G1.png");
