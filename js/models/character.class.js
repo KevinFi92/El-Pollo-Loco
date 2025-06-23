00244e03ef01651b106e136a70cd0aad829280fb
KevinFi92 <168017579+KevinFi92@users.noreply.github.com>
2025-06-23 13:16:31 +0200

    added Idle animation

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -5,7 +5,8 @@ class Character extends MoveableObject {
     jumpingImgs = ["img/2_character_pepe/3_jump/J-31.png", "img/2_character_pepe/3_jump/J-32.png", "img/2_character_pepe/3_jump/J-33.png", "img/2_character_pepe/3_jump/J-34.png", "img/2_character_pepe/3_jump/J-35.png", "img/2_character_pepe/3_jump/J-36.png", "img/2_character_pepe/3_jump/J-37.png", "img/2_character_pepe/3_jump/J-38.png", "img/2_character_pepe/3_jump/J-39.png",];
     deathImgs = ["img/2_character_pepe/5_dead/D-51.png", "img/2_character_pepe/5_dead/D-52.png", "img/2_character_pepe/5_dead/D-53.png", "img/2_character_pepe/5_dead/D-54.png", "img/2_character_pepe/5_dead/D-55.png", "img/2_character_pepe/5_dead/D-56.png", "img/2_character_pepe/5_dead/D-57.png",];
     hurtImgs = ["img/2_character_pepe/4_hurt/H-41.png", "img/2_character_pepe/4_hurt/H-42.png", "img/2_character_pepe/4_hurt/H-43.png",];
-    idleImgs = ["img/2_character_pepe/1_idle/long_idle/I-11.png", "img/2_character_pepe/1_idle/long_idle/I-12.png", "img/2_character_pepe/1_idle/long_idle/I-13.png", "img/2_character_pepe/1_idle/long_idle/I-14.png", "img/2_character_pepe/1_idle/long_idle/I-15.png", "img/2_character_pepe/1_idle/long_idle/I-16.png", "img/2_character_pepe/1_idle/long_idle/I-17.png", "img/2_character_pepe/1_idle/long_idle/I-18.png", "img/2_character_pepe/1_idle/long_idle/I-19.png", "img/2_character_pepe/1_idle/long_idle/I-20.png",
+    idleImgs = ["img/2_character_pepe/1_idle/idle/I-1.png","img/2_character_pepe/1_idle/idle/I-2.png","img/2_character_pepe/1_idle/idle/I-3.png","img/2_character_pepe/1_idle/idle/I-4.png","img/2_character_pepe/1_idle/idle/I-5.png","img/2_character_pepe/1_idle/idle/I-6.png","img/2_character_pepe/1_idle/idle/I-7.png","img/2_character_pepe/1_idle/idle/I-8.png","img/2_character_pepe/1_idle/idle/I-9.png","img/2_character_pepe/1_idle/idle/I-10.png"]
+    idleLongImgs = ["img/2_character_pepe/1_idle/long_idle/I-11.png", "img/2_character_pepe/1_idle/long_idle/I-12.png", "img/2_character_pepe/1_idle/long_idle/I-13.png", "img/2_character_pepe/1_idle/long_idle/I-14.png", "img/2_character_pepe/1_idle/long_idle/I-15.png", "img/2_character_pepe/1_idle/long_idle/I-16.png", "img/2_character_pepe/1_idle/long_idle/I-17.png", "img/2_character_pepe/1_idle/long_idle/I-18.png", "img/2_character_pepe/1_idle/long_idle/I-19.png", "img/2_character_pepe/1_idle/long_idle/I-20.png",
 
     ]
     currentImg = 0;
@@ -96,9 +97,11 @@ class Character extends MoveableObject {
         let idle = setInterval(() => {
             time += 0.1;
             if (time >= 5) {
-                this.movementAnimation(this.idleImgs);
+                this.movementAnimation(this.idleLongImgs);
                 world.playSound(this.snoringSound);
-            }
+            }else(
+                this.movementAnimation(this.idleImgs)
+            )
             if (world.keyboard.right || world.keyboard.left || world.keyboard.up || world.keyboard.space) {
                 clearInterval(idle);
                 this.setIdleTimer();
